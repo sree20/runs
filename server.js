ac40e15f3287e7d7abd6804725100e5bee074471
Ratakonda, Sreekanth <Sreekanth-Ratakonda@idexx.com>
2016-08-22 14:48:57 -0400

    Add port listening to server.js file

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express'); // require express package
+var app = express(); // create application variable
+
+app.listen(3000,function(){  //start app listening on port 3000
+  console.log('example app listening on port 3000!');
+})
