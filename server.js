4a8336c8ca916d9bf931099c3e26e49c9146ee76
Ratakonda, Sreekanth <Sreekanth-Ratakonda@idexx.com>
2016-08-22 15:23:19 -0400

    Committing the View file to server for MVC Pattern

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express'); // require express package
 var app = express(); // create application variable
 
 app.get('/',function(request,response){
-  response.send('hi hello world');
+//  response.send('hi hello world!!!');
+response.render('index.ejs');
 });
 
 app.listen(3000,function(){  //start app listening on port 3000
