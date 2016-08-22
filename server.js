bc706582b13e36f03fff2c72e83803749f6f7f1b
Ratakonda, Sreekanth <Sreekanth-Ratakonda@idexx.com>
2016-08-22 14:57:00 -0400

    Added the get method of Express

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express'); // require express package
 var app = express(); // create application variable
 
+app.get('/',function(request,response){
+  response.send('hi hello world');
+});
+
 app.listen(3000,function(){  //start app listening on port 3000
   console.log('example app listening on port 3000!');
-})
+});
