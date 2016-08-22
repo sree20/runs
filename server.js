04a07410e4bc1fce0cf51e7fb880861619f6dd60
Ratakonda, Sreekanth <Sreekanth-Ratakonda@idexx.com>
2016-08-22 15:43:01 -0400

    Added the Data to the view by fakeArray

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express'); // require express package
 var app = express(); // create application variable
 
+var fakeArray = require('./models/fakearray.js');
+console.log(fakeArray);
 app.get('/',function(request,response){
 //  response.send('hi hello world!!!');
-response.render('index.ejs');
+response.render('index.ejs',{dataArray:fakeArray});
 });
 
 app.listen(3000,function(){  //start app listening on port 3000
