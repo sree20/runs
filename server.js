4b0618b298c76ab5408301c5884c06cf3aa56fce
Ratakonda, Sreekanth <Sreekanth-Ratakonda@idexx.com>
2016-08-22 16:34:26 -0400

    Changed the port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'); // require express package
 var app = express(); // create application variable
-
+var PORT = process.env.PORT || 3000;
 var fakeArray = require('./models/fakearray.js');
 console.log(fakeArray);
 app.get('/',function(request,response){
@@ -8,6 +8,6 @@ app.get('/',function(request,response){
 response.render('index.ejs',{dataArray:fakeArray});
 });
 
-app.listen(3000,function(){  //start app listening on port 3000
+app.listen(PORT,function(){  //start app listening on port 3000
   console.log('example app listening on port 3000!');
 });
