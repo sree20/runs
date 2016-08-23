77d1602be04d014c10a112f2b4ce816561e22feb
Ratakonda, Sreekanth <Sreekanth-Ratakonda@idexx.com>
2016-08-23 09:02:44 -0400

    added the middleware in server.js

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var app = express(); // create application variable
 var PORT = process.env.PORT || 3000;
 var fakeArray = require('./models/fakearray.js');
 console.log(fakeArray);
+app.use(function(req,res,next){
+  console.log('middleware 1');
+  next();
+});
+app.use(function(req,res,next){
+  console.log('middleware 2');
+  next();
+});
+app.get('/foo',function(req,res){
+  res.send('works');
+});
 app.get('/',function(request,response){
 //  response.send('hi hello world!!!');
 response.render('index.ejs',{dataArray:fakeArray});
